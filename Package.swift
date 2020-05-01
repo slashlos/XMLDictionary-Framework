2f39997ad36ab6c5221ed88a65edf59709b3da4a
cd santiago <slashlos@gmail.com>
2020-05-01 15:16:20 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
-	    .library(name: "XMLDictionary", targets: ["XMLDictionary"],
+	    .library(name: "XMLDictionary", targets: ["XMLDictionary"]),
 	],
         dependencies: [
             // Dependencies declare other packages that this package depends on.
