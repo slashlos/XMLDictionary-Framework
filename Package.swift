6b10600df2d5d13a6dfab08bc460e701cced0bda
cd santiago <slashlos@gmail.com>
2020-05-01 15:18:51 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
 	],
         dependencies: [
             // Dependencies declare other packages that this package depends on.
-            .package(url: "https://github.com/nicklockwood/XMLDictionary", from: "1.4.1"),
+            .package(url: "https://github.com/slashlos/XMLDictionary", from: "1.4.1"),
         ],
         targets: [
             // Targets are the basic building blocks of a package. A target can define a module or a test suite.
