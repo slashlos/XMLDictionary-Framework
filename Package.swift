cd855a20a3ce93bb73c0bf6b451c0523bf0637fe
cd santiago <slashlos@gmail.com>
2026-07-17 16:00:12 -0400

    downcase package identity name

--- Package.swift
+++ Package.swift
@@ -1,26 +1,20 @@
-// swift-tools-version:5.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
-
+// swift-tools-version:5.5
 import PackageDescription
 
 let package = Package(
-    name: "XMLDictionary",
-    platforms: [
-        .macOS, .iOS, .tvOS,
-    ],
+    name: "xmldictionary-framework", // <-- Must be exact lowercase string Xcode expects
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-        .library(name: "XMLDictionary", targets: ["XMLDictionary"]),
-        ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
+        .library(
+            name: "XMLDictionary", 
+            targets: ["XMLDictionary"]
+        )
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(name: "XMLDictionary", dependencies: [],
-                path: "XMLDictionary",
-                publicHeadersPath: ".")
+        .target(
+            name: "XMLDictionary",
+            path: "XMLDictionary", // Point to folder containing XMLDictionary.h/.m
+            publicHeadersPath: "."
+        )
     ]
 )
 
